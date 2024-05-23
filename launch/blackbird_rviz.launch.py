d3c049d5b9adda35b52bd0bf9e6d6e2a565c1c47
aihoque2 <aihoque2@illinois.edu>
2024-05-23 00:07:48 -0700

    blackbird rviz visualized

--- launch/blackbird_rviz.launch.py
+++ launch/blackbird_rviz.launch.py
@@ -11,7 +11,7 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
     urdf_tutorial_path = get_package_share_path('ETH_biped')
     default_model_path = urdf_tutorial_path / 'urdf/blackbird.urdf'
-    default_rviz_config_path = urdf_tutorial_path / 'rviz/basic.rviz'
+    default_rviz_config_path = urdf_tutorial_path / 'rviz/blackbird.rviz'
 
     gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
                                     description='Flag to enable joint_state_publisher_gui')
