de6678a7a4962f3c9275d27d3302a86239b34ac4
aihoque2 <aihoque2@illinois.edu>
2024-05-28 01:27:12 -0700

    ingition launch

---
+++ launch/blackbird_ign.launch.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import SetEnvironmentVariable, RegisterEventHandler
+
+
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnProcessExit
+
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+import xacro
+
+"""
+blackbird_ign.launch.py
+"""
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    eth_biped_path = os.path.join(
+        get_package_share_directory('ETH_biped')
+    )
+
+
+    urdf_file = os.path.join(eth_biped_path, 'urdf', 'blackbird.urdf')
+    doc = xacro.parse(open(urdf_file))
+    params = {'robot_description': doc.toxml()}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-string', doc.toxml(),
+                   '-name', 'blackbird',
+                   '-allow_renaming', 'true'],
+    )
+
+    # load_joint_state_broadcaster = ExecuteProcess(
+    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+    #          'joint_state_broadcaster'],
+    #     output='screen'
+    # )
+
+    # load_joint_effort_controller = ExecuteProcess(
+    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'effort_controller'],
+    #     output='screen'
+    # )
+
+    return LaunchDescription([
+        # Launch gazebo environment
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('ros_ign_gazebo'),
+                              'launch', 'ign_gazebo.launch.py')]),
+            launch_arguments=[('gz_args', [' -r -v 3 empty.sdf'])]),
+
+        # RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=spawn_entity,
+        #         on_exit=[load_joint_state_broadcaster],
+        #     )
+        # ),
+        # RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=load_joint_state_broadcaster,
+        #         on_exit=[load_joint_effort_controller],
+        #     )
+        # ),
+
+        robot_state_publisher,
+        spawn_entity,
+        # Launch Arguments
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='If true, use simulated clock'),
+    ])
