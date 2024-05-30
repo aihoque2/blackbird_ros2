36fa23cc9d92ae3b4f8efa57b6551630974ae5bb
aihoque2 <aihoque2@illinois.edu>
2024-05-30 02:08:43 -0700

    blackbird launches in ignition sim: ign_ros2_control error bug: [Err] [SystemLoader.cc:94] Failed to load system plugin [ign_ros2_control-system] : couldn't find shared library.wq

---
+++ README.md
@@ -0,0 +1,5 @@
+# BIPEDAL WALKER SIMULATION WITH IGNITION GAZEBO
+
+please run `export IGN_GAZEBO_RESOURCE_PATH=/home/ahoque245/workspaces/ros2_ws/src/ETH_biped`
+
+then run `ros2 launch ETH_biped blackbird_ign.launch.py`
--- launch/blackbird_rviz.launch.py
+++ launch/blackbird_rviz.launch.py
@@ -10,7 +10,7 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     pkg_path = get_package_share_path('ETH_biped')
-    default_model_path = pkg_path / 'urdf/blackbird_ign.urdf'
+    default_model_path = pkg_path / 'urdf/blackbird.urdf'
     default_rviz_config_path = pkg_path / 'rviz/blackbird.rviz'
 
     gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
